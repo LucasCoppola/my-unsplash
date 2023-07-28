2cef05a22dc72d9085056a6e7c60208fbb3bc2dd
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-28 17:46:56 -0300

    removed nextAuthOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,14 +2,11 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 // import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from "next-auth/providers/google"
 
-const authOptions: NextAuthOptions = {
-	// adapter: PrismaAdapter(prisma),
+export default NextAuth({
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 	],
-}
-
-export default NextAuth(authOptions)
+})
