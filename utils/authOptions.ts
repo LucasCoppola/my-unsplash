8f65bd3cb29756d7d3386b6ae1bb2bed38312631
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-28 19:36:23 -0300

    added secret

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -9,5 +9,6 @@ export const authOptions: NextAuthOptions = {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
 		})
-	]
+	],
+	secret: process.env.NEXTAUTH_SECRET as string
 }
