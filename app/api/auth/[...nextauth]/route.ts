ff05b36426f97c1bc9f07b5954fa075774c4d23e
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-28 18:24:54 -0300

    split auth options to a different file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-// import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import GoogleProvider from "next-auth/providers/google"
+import { authOptions } from '@/utils/authOptions'
+import NextAuth from 'next-auth/next'
 
-export const authOptions: NextAuthOptions = {
-	// adapter: PrismaAdapter(prisma),
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		}),
-	],
-}
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
