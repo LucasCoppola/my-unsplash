db31a8032a02f09dd7aa701984a8f7f9134ef2d9
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-07-30 18:27:15 -0300

    after log out redirect to '/'

--- components/ui/user-dropdown.tsx
+++ components/ui/user-dropdown.tsx
@@ -1,12 +1,15 @@
-"use client"
+'use client'
 
-import { useState } from "react"
-import { signOut } from "next-auth/react"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { signOut } from 'next-auth/react'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger
+} from '@/components/ui/popover'
 
-import Image from "next/image"
-import { Session } from "next-auth"
-import { LogOut } from "lucide-react"
+import Image from 'next/image'
+import { Session } from 'next-auth'
+import { LogOut } from 'lucide-react'
 
 export default function UserDropdown({ session }: { session: Session }) {
 	const { email, image } = session?.user || {}
@@ -19,7 +22,10 @@ export default function UserDropdown({ session }: { session: Session }) {
 				<PopoverTrigger className="flex h-8 w-8 items-center justify-center overflow-hidden rounded-full border border-gray-300 transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9">
 					<Image
 						alt={email}
-						src={image || `https://avatars.dicebear.com/api/micah/${email}.svg`}
+						src={
+							image ||
+							`https://avatars.dicebear.com/api/micah/${email}.svg`
+						}
 						width={40}
 						height={40}
 					/>
@@ -28,7 +34,10 @@ export default function UserDropdown({ session }: { session: Session }) {
 					<div className="w-full rounded-md bg-white p-2 sm:w-56">
 						<button
 							className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
-							onClick={() => signOut()}>
+							onClick={() =>
+								signOut({ redirect: true, callbackUrl: '/' })
+							}
+						>
 							<LogOut className="h-4 w-4" />
 							<p className="text-sm">Logout</p>
 						</button>
