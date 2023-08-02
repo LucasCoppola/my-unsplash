798c8eea0c7b5d94467b13d49c9340091b98dd4e
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-02 10:57:36 -0300

    removed sample data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,44 +4,6 @@ import { getServerSession } from 'next-auth/next'
 import { redirect } from 'next/navigation'
 import { getImages } from '../server'
 
-// const images = [
-// 	{
-// 		id: 1,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1689975352/Store/zygsbm2ra0w6cf7tyy9d.jpg',
-// 		label: 'label'
-// 	},
-// 	{
-// 		id: 2,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1689953788/sample.jpg',
-// 		label: 'label'
-// 	},
-// 	{
-// 		id: 3,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1688737914/powerlifting_x8zwhg.jpg',
-// 		label: 'label'
-// 	},
-// 	{
-// 		id: 4,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1688737804/001-Rogue-Fitness-Calisthenics-Parallettes-004-opt_prexfz.jpg',
-// 		label: 'label'
-// 	},
-// 	{
-// 		id: 5,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1688737328/bg-image_n18nut.jpg',
-// 		label: 'label'
-// 	},
-// 	{
-// 		id: 6,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1676568161/YelpCamp/nv2gaq8uilqxg17tp6lr.jpg',
-// 		label: 'label'
-// 	},
-// 	{
-// 		id: 7,
-// 		src: 'https://res.cloudinary.com/dotpfjpno/image/upload/v1675862370/YelpCamp/cblohpx9wtwgnd5gzets.jpg',
-// 		label: 'label'
-// 	}
-// ]
-
 export default async function Dashboard() {
 	const { images } = await getImages()
 	const session = await getServerSession(authOptions)
