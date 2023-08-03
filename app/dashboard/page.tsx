b0018a94e63018fb1fcf7311e3f9bfd031655e6f
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-08-03 18:53:33 -0300

    empty page image

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from '@/utils/authOptions'
 import { getServerSession } from 'next-auth/next'
 import { redirect } from 'next/navigation'
 import { getImages } from '../server'
+import Image from 'next/image'
 
 export default async function Dashboard() {
 	const session = await getServerSession(authOptions)
@@ -14,11 +15,26 @@ export default async function Dashboard() {
 
 	return (
 		<main className="mt-8 flex flex-col items-center justify-between p-6">
-			<div className="columns-auto gap-6 md:columns-3">
-				{images?.map(({ src, id, label }) => (
-					<ImageComponent key={id} src={src} label={label} id={id} />
-				))}
-			</div>
+			{images?.length ? (
+				<div className="columns-auto gap-6 md:columns-3">
+					{images?.map(({ src, id, label }) => (
+						<ImageComponent
+							key={id}
+							src={src}
+							label={label}
+							id={id}
+						/>
+					))}
+				</div>
+			) : (
+				<Image
+					src="/empty-page.svg"
+					alt="empty page image"
+					width={500}
+					height={500}
+					className="m-auto"
+				/>
+			)}
 		</main>
 	)
 }
